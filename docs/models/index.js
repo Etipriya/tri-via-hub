80ed48c9672594023db282d23d61b0e0f7589966
Vilma Qerama <vilma.qerama@hotmail.com>
2021-06-11 17:17:14 +0100

    add index.js for models

---
+++ docs/models/index.js
@@ -0,0 +1,36 @@
+const User = require("./user");
+const Quiz = require("./quiz");
+const Question = require("./question");
+const Answer = require("./answer");
+const Score = require("./score");
+
+User.hasMany(Quiz, {
+  foreignKey: "user_id",
+});
+
+User.hasMany(Score, {
+  foreignKey: "user_id",
+});
+
+Quiz.hasMany(Score, {
+  foreignKey: "quiz_id",
+});
+Question.hasMany(Answer, {
+  foreignKey: "answer_id",
+});
+
+Quiz.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+Score.belongsTo(User, {
+  foreignKey: "user_id",
+});
+Score.belongsTo(Quiz, {
+  foreignKey: "quiz_id",
+});
+Answer.belongsTo(Question, {
+  foreignKey: "question_id",
+});
+
+module.exports = { User, Quiz, Question, Answer, Score };
