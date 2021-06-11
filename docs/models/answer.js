0910b53296e49decc3b82f668e0b2bc204cc5f2f
Vilma Qerama <vilma.qerama@hotmail.com>
2021-06-11 17:00:27 +0100

    add answer.js model

---
+++ docs/models/answer.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require("sequelize");
+
+const sequelize = require("../config/connection");
+
+const schema = {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  option: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      len: [2, 50],
+    },
+  },
+  correct_answer: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+  },
+
+  question_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: "question",
+      key: "id",
+    },
+  },
+};
+
+const options = {
+  sequelize,
+  modelName: "answer",
+  freezeTableName: true,
+  timestamps: true,
+  underscored: true,
+};
+
+class Answer extends Model {}
+
+Answer.init(schema, options);
+
+module.exports = Answer;
