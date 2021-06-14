b7e87b7f1451e42e6b14ca864ba1820409f141d0
Leon <leonwheeler08@gmail.com>
2021-06-14 06:59:46 +0100

    Edited single quiz function to see user scores.

--- src/index.js
+++ src/index.js
@@ -4,29 +4,10 @@ const connection = require("./config/connection");
 const { User, Quiz, Question, Answer, Score } = require("./models");
 
 const getAllQuizzes = async () => {
-  await connection.sync();
-
-  const allQuizzes = await Quiz.findAll({
-    include: [
-      { model: User, attributes: ["username"] },
-      {
-        model: Question,
-        attributes: ["question", "correct_option"],
-        include: { model: Answer },
-      },
-    ],
-  });
-  const quizzes = allQuizzes.map((quiz) => quiz.get({ plain: true }));
-  console.log(util.inspect(quizzes, { depth: null }));
-};
-
-const getSingleQuiz = async () => {
   try {
     await connection.sync();
 
-    const id = 2;
-
-    const singleQuiz = await Quiz.findByPk(id, {
+    const allQuizzes = await Quiz.findAll({
       include: [
         { model: User, attributes: ["username"] },
         {
@@ -36,10 +17,54 @@ const getSingleQuiz = async () => {
         },
       ],
     });
+    const quizzes = allQuizzes.map((quiz) => quiz.get({ plain: true }));
+    console.log(util.inspect(quizzes, { depth: null }));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getSingleQuiz = async () => {
+  try {
+    await connection.sync();
+
+    const id = 3;
+
+    const singleQuiz = await Quiz.findByPk(id, {
+      include: [
+        { model: User, attributes: ["username"] },
+        {
+          model: Question,
+          attributes: ["question", "correct_option"],
+          include: { model: Answer },
+        },
+        { model: Score, include: { model: User, attributes: ["username"] } },
+      ],
+    });
 
     const formattedQuiz = singleQuiz.get({ plain: true });
 
     console.log(util.inspect(formattedQuiz, { depth: null }));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getQuizByCategory = async () => {
+  try {
+    await connection.sync();
+
+    const category = "Entertainment: Video Games";
+
+    const quizzes = await Quiz.findAll({
+      where: {
+        category,
+      },
+    });
+
+    const formattedQuizzes = quizzes.map((quiz) => quiz.get({ plain: true }));
+
+    console.log(formattedQuizzes);
   } catch (error) {
     console.log(error);
   }
@@ -67,13 +92,36 @@ const getQuestionsByQuizId = async () => {
       question.get({ plain: true })
     );
 
-    console.log(util.inspect(formattedQuestions, { depth: null }));
+    console.log(formattedQuestions);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getUserQuizzes = async () => {
+  try {
+    await connection.sync();
+
+    const id = 2;
+
+    const user = await User.findByPk(id, {
+      include: [
+        { model: Quiz, attributes: ["title", "category", "difficulty"] },
+      ],
+    });
+
+    const formattedUser = user.get({ plain: true });
+
+    console.log(util.inspect(formattedUser, { depth: null }));
   } catch (error) {
     console.log(error);
   }
 };
 
 // getAllQuizzes();
-// getSingleQuiz();
+getSingleQuiz();
+// getQuizByCategory();
 
-getQuestionsByQuizId();
+// getQuestionsByQuizId();
+
+// getUserQuizzes();
