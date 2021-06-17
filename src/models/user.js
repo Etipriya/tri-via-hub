ddb37d235a40fdb6daeaa8ab7faf52bd420479d7
nazimrasheed <mr.rasheed1991@gmail.com>
2021-06-17 20:14:02 +0100

    created hooks to encrypt password with bcrypt

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,7 @@
 const { Model, DataTypes } = require("sequelize");
 
+const bcrypt = require("bcrypt");
+
 const sequelize = require("../config/connection");
 
 const schema = {
@@ -45,7 +47,28 @@ const schema = {
   },
 };
 
+const hooks = {
+  beforeCreate: async (newUser) => {
+    newUser.password = await bcrypt.hash(newUser.password, 10);
+  },
+
+  beforeBulkCreate: async (users) => {
+    const promises = users.map((user) => {
+      return bcrypt.hash(user.password, 10);
+    });
+    const passwords = await Promise.all(promises);
+    passwords.forEach((password, index) => {
+      users[index].password = password;
+    });
+  },
+  beforeUpdate: async (updatedUser) => {
+    updatedUser.password = await bcrypt.hash(updatedUser.password, 10);
+    return updatedUser;
+  },
+};
+
 const options = {
+  hooks,
   sequelize,
   modelName: "user",
   freezeTableName: true,
