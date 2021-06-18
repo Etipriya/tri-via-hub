27baaf93f7fe7c9266955878915c72b906d6d7df
Leon <leonwheeler08@gmail.com>
2021-06-18 18:46:01 +0100

    Added API functionality to create a new quiz (just title) with linked user ID.

--- src/routes/api/quizRoutes.js
+++ src/routes/api/quizRoutes.js
@@ -4,6 +4,7 @@ const {
   getAllQuizzes,
   getQuizById,
   getQuizByTitle,
+  createQuiz,
 } = require("../../controllers/api/quiz");
 
 const router = Router();
@@ -11,5 +12,6 @@ const router = Router();
 router.get("/search", getQuizByTitle);
 router.get("/:id", getQuizById);
 router.get("/", getAllQuizzes);
+router.post("/create", createQuiz);
 
 module.exports = router;
