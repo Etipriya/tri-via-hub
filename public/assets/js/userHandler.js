8d8ae4962b4354537329db41f21576e065066685
Leon <leonwheeler08@gmail.com>
2021-06-18 18:16:30 +0100

    Added confirm password check to sign up.

--- public/assets/js/userHandler.js
+++ public/assets/js/userHandler.js
@@ -28,40 +28,43 @@ const handleLogin = async (event) => {
 const handleSignup = async (event) => {
   event.preventDefault();
 
-  console.log("Here");
-
   const first_name = $("#first_name").val();
   const last_name = $("#last_name").val();
   const username = $("#username").val();
   const email = $("#email").val();
   const password = $("#password").val();
+  const confirmPassword = $("#confirmPassword").val();
 
   if (!first_name || !last_name || !username || !email || !password) {
     console.log("You must complete all fields");
     return;
   }
 
-  const options = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    redirect: "follow",
-    body: JSON.stringify({
-      first_name,
-      last_name,
-      username,
-      email,
-      password,
-    }),
-  };
+  if (password === confirmPassword) {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      redirect: "follow",
+      body: JSON.stringify({
+        first_name,
+        last_name,
+        username,
+        email,
+        password,
+      }),
+    };
 
-  const response = await fetch("/auth/signup", options);
+    const response = await fetch("/auth/signup", options);
 
-  if (response.status !== 201) {
-    console.log("FAILED TO CREATE USER");
+    if (response.status !== 201) {
+      console.log("FAILED TO CREATE USER");
+    } else {
+      window.location.replace("/dashboard");
+    }
   } else {
-    window.location.replace("/dashboard");
+    console.log("PASSWORDS DO NOT MATCH!");
   }
 };
 
