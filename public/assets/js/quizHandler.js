197a4759745ddf94e03be5b4d6d9e5f7401f5746
Leon <leonwheeler08@gmail.com>
2021-06-19 14:30:27 +0100

    Can now use the search bar to search for a quiz by title

---
+++ public/assets/js/quizHandler.js
@@ -0,0 +1,26 @@
+const handleQuizSearch = async (event) => {
+  event.preventDefault();
+
+  const searchQuery = $("#searchQuery").val();
+
+  const options = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    redirect: "follow",
+    query: JSON.stringify({
+      title: searchQuery,
+    }),
+  };
+
+  const response = await fetch("/api/quiz/search", options);
+
+  if (response.status !== 200) {
+    console.log("Failed to search!");
+  } else {
+    window.location.replace(`/quiz/search?title=${searchQuery}`);
+  }
+};
+
+$("#quizSearch").submit(handleQuizSearch);
--- src/controllers/view/privateRender.js
+++ src/controllers/view/privateRender.js
@@ -1,3 +1,5 @@
+const { Op } = require("sequelize");
+
 const { Quiz, User } = require("../../models");
 
 const renderDashboardPage = (req, res) => {
@@ -12,9 +14,9 @@ const renderMainQuizPage = async (req, res) => {
       order: [["createdAt", "DESC"]],
     });
     // Getting a plain version of the JSON data (just data we inputted)
-    let formattedQuizzes = allQuizzes.map((quiz) => quiz.get({ plain: true }));
-
-    formattedQuizzes = formattedQuizzes.slice(0, 8);
+    let formattedQuizzes = allQuizzes
+      .map((quiz) => quiz.get({ plain: true }))
+      .slice(0, 8);
 
     res.render("quizzes", { formattedQuizzes });
   } catch (err) {
@@ -28,9 +30,42 @@ const renderQuizPageById = (req, res) => {
   res.render("individual-quiz");
 };
 
+const renderSearchedQuizzes = async (req, res) => {
+  try {
+    const { title, category } = req.query;
+
+    let searchTerm;
+    let columnName;
+
+    if (title) {
+      searchTerm = title;
+      columnName = "title";
+    } else {
+      searchTerm = category;
+      columnName = "category";
+    }
+
+    const quizzes = await Quiz.findAll({
+      where: {
+        [columnName]: {
+          [Op.like]: `%${searchTerm}%`,
+        },
+      },
+      include: [{ model: User, attributes: ["username"] }],
+    });
+
+    const formattedQuizzes = quizzes.map((quiz) => quiz.get({ plain: true }));
+
+    res.render("searchedQuizzes", { formattedQuizzes });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
 module.exports = {
   renderDashboardPage,
   renderMainQuizPage,
   renderQuizPageById,
   renderCreateQuizPage,
+  renderSearchedQuizzes,
 };
