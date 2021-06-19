04d3143ed3c396ea16c04ced16a2a37fc5c6e84e
Leon <leonwheeler08@gmail.com>
2021-06-19 13:58:34 +0100

    8 most recent quizzes are now rendered when the quizzes page is loaded

--- src/controllers/view/privateRender.js
+++ src/controllers/view/privateRender.js
@@ -1,8 +1,25 @@
+const { Quiz, User } = require("../../models");
+
 const renderDashboardPage = (req, res) => {
   res.render("dashboard");
 };
-const renderMainQuizPage = (req, res) => {
-  res.render("quizzes");
+const renderMainQuizPage = async (req, res) => {
+  try {
+    const allQuizzes = await Quiz.findAll({
+      include:
+        // In the JSON it returns it will include username of the creator of the quiz
+        { model: User, attributes: ["username"] },
+      order: [["createdAt", "DESC"]],
+    });
+    // Getting a plain version of the JSON data (just data we inputted)
+    let formattedQuizzes = allQuizzes.map((quiz) => quiz.get({ plain: true }));
+
+    formattedQuizzes = formattedQuizzes.slice(0, 8);
+
+    res.render("quizzes", { formattedQuizzes });
+  } catch (err) {
+    console.error(err);
+  }
 };
 const renderCreateQuizPage = (req, res) => {
   res.render("create-quiz");
