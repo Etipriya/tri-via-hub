1d01170e59e7d83fd57d2b1e5579d7be9472f1ca
Leon <leonwheeler08@gmail.com>
2021-06-24 20:35:57 +0100

    Modal is now showing when timer runs out or finish button is clicked.

--- public/assets/js/gameLogic.js
+++ public/assets/js/gameLogic.js
@@ -1,51 +1,56 @@
 let score = 0;
 let timerValue = $(".quizQuestion").length * 10;
+// let timerValue = 2;
 
 const isAnswer = async (event) => {
-  const button = $(event.target);
+  const answer = $(event.target);
   const question = $(event.currentTarget);
 
-  const givenAnswer = button.attr("data-answer");
+  if (answer.is("li")) {
+    console.log(answer);
+    console.log(question);
 
-  const options = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    redirect: "follow",
-    body: JSON.stringify({
-      id: question.attr("id"),
-      givenAnswer,
-    }),
-  };
+    const givenAnswer = answer.attr("data-answer");
 
-  const response = await fetch("/api/quiz/check-answer", options);
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      redirect: "follow",
+      body: JSON.stringify({
+        id: question.attr("id"),
+        givenAnswer,
+      }),
+    };
 
-  const { success } = await response.json();
+    const response = await fetch("/api/quiz/check-answer", options);
 
-  if (response.status !== 200) {
-    console.log("Failed to get answer");
-    // to do error handling
-  } else {
-    if (success) {
-      question.find("ul").replaceWith(
-        `<div class="alert alert-success" role="alert">
+    const { success } = await response.json();
+
+    if (response.status !== 200) {
+      console.log("Failed to get answer");
+      // to do error handling
+    } else {
+      if (success) {
+        question.find("ul").replaceWith(
+          `<div class="alert alert-success" role="alert">
           Correct
         </div>`
-      );
-      score += 3;
-    } else {
-      question.find("ul").replaceWith(
-        `<div class="alert alert-danger" role="alert">
+        );
+        score += 3;
+      } else {
+        question.find("ul").replaceWith(
+          `<div class="alert alert-danger" role="alert">
           Incorrect
         </div>`
-      );
-      score -= 1;
+        );
+        score -= 1;
+      }
     }
-  }
-
-  if (score < 0) {
-    score = 0;
+    if (score < 0) {
+      score = 0;
+    }
   }
 };
 
@@ -71,6 +76,7 @@ const sendScore = async () => {
   if (response.status !== 201) {
     console.log("Failed to save score");
   } else {
+    window.location.replace("/dashboard");
     console.log("score saved");
   }
 };
@@ -79,25 +85,33 @@ const sendScore = async () => {
 const startTimer = () => {
   const callback = () => {
     // if timer is active and game is still active
-    $("#timer").text(timerValue);
-    console.log(timerValue);
 
     if (timerValue > 0) {
-      timerValue = timerValue - 1;
+      timerValue -= 1;
     }
 
+    $("#timer").text(timerValue);
+
     if (timerValue === 0) {
+      console.log("here");
       clearInterval(timer);
-      sendScore();
+      // sendScore();
+      $("#quiz-modal").modal("show");
     }
   };
   const timer = setInterval(callback, 1000);
 };
 
 const startQuiz = () => {
+  $("#quiz-modal").modal("hide");
   startTimer();
+};
+
+const showModal = () => {
+  $("#quiz-modal").modal("show");
 };
 
 $(".question").click(isAnswer);
 $(document).ready(startQuiz);
-$("[name='finish-btn']").click(sendScore);
+$("#submit-score-btn").click(sendScore);
+$("[name='finish-btn']").click(showModal);
